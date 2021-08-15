5a6ffe188b9341b391675006332e8c4c407114c5
yugutou947 <1965372693@qq.com>
2021-08-15 20:44:10 +0800

    快速排序练习

--- main.c
+++ main.c
@@ -1,9 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define  NUM 40
+#define  LINE 6
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void fillarry(int num, double * arry)
+{
+	for(int index = 0; index < num; index++)
+	{
+		arry[index] = rand() / (rand() + 0.1);
+	}
+}
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!");
+void showarry(int num, const double * arry)
+{
+	for(int index = 0; index < num; index++)
+	{
+		printf("%6.2lf", arry[index]);
+		if(index % 6 == 5)
+		{
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
+
+int mycompare(const void * p1, const void * p2)
+{
+	const double * a1 = (const double *)p1;
+	const double * a2 = (const double *)p2;
+	
+	if(*a1 < *a2)
+	{
+		return -1;
+	}
+	else if(*a1 == *a2)
+	{
+		return 0;
+	}
+	else 
+	return 1;
+}
+
+int main() 
+{
+	double arryy[NUM];
+	fillarry(NUM, arryy);
+	showarry(NUM, arryy);
+	qsort(arryy, NUM, sizeof(double), mycompare);
+	showarry(NUM, arryy);
+	
 	return 0;
 }
